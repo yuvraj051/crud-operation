4f4b02f58fa93c720a8a9d44eff90e5d7a6bf81c
yuvraj051 <vyuvraj051@gmail.com>
2025-06-10 12:06:23 +0530

    frontend create with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-9xl bg-amber-950">hello</h1>
+    </>
+  );
+}
+
+export default App;
