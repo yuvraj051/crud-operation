5dd6dfdcf7b25eaaff9f518284a295739f0917bb
shivam gauswami <155231073+shivm166@users.noreply.github.com>
2025-06-10 16:32:48 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import db from "../backend/config/db.js"; // Make sure this path is correct
 import route from "./routes/route.js";
+import cors from "cors";
 
 dotenv.config(); // Should come before using environment variables
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
