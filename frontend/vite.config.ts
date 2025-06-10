1628cf20b5b379590aba09976a9d198b4d819f09
yuvraj051 <vyuvraj051@gmail.com>
2025-06-10 11:46:55 +0530

    remove ts setup

--- frontend/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
