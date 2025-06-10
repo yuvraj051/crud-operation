c7e562744b97026f11bf148ed69f2005ccec090f
shivam gauswami <155231073+shivm166@users.noreply.github.com>
2025-06-10 12:50:36 +0530

    user created

--- backend/controllers/controlller.js
+++ backend/controllers/controlller.js
@@ -1,14 +1,36 @@
 import User from "../model/model.js";
+import bcrypt from "bcrypt";
 
 export const create = async (req, res) => {
   try {
     const { name, email, password, gender } = req.body;
+
     if (!name || !email || !password || !gender) {
-      return res.status(401).json();
+      return res.status(400).json({ message: "Something is missing..." });
     }
+
     const user = await User.findOne({ email });
+    if (user) {
+      return res.status(409).json({ message: "User already exists..." });
+    }
+
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashpassword,
+      gender,
+    });
+
+    res
+      .status(201)
+      .json({ message: "User created successfully!", user: newUser });
   } catch (error) {
-    res.status(400).json({ error, message: "internal server error..." });
+    res
+      .status(500)
+      .json({ error: error.message, message: "Internal server error..." });
+    console.log(error);
   }
 };
 export const read = async (req, res) => {
